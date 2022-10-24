80995ac4bad3f1aea7ccb59e4c64078ba7c2d612
lekynam2000 <lekynam2000@gmail.com>
2022-10-24 14:26:46 +0800

    test batch training

--- utils.py
+++ utils.py
@@ -36,19 +36,22 @@ class QueueMask():
 
 
 def mask_generator(shadow, shadow_free):
-    im_f = to_gray(to_pil(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu()))
-    im_s = to_gray(to_pil(((shadow.data.squeeze(0) + 1.0) * 0.5).cpu()))
+    assert shadow.size()[0]==shadow_free.size()[0]
+    masks=[]
+    for i in range(shadow.size()[0]):
+        im_f = to_gray(to_pil(((shadow_free.data[i] + 1.0) * 0.5).cpu()))
+        im_s = to_gray(to_pil(((shadow.data[i] + 1.0) * 0.5).cpu()))
 
-    ps(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu(),"original")
-    psn(to_pil(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu()),"to_pil")
-    psn(to_gray(to_pil(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu())),"to_gray")
+        # ps(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu(),"original")
+        # psn(to_pil(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu()),"to_pil")
+        # psn(to_gray(to_pil(((shadow_free.data.squeeze(0) + 1.0) * 0.5).cpu())),"to_gray")
 
-    diff = (np.asarray(im_f, dtype='float32')- np.asarray(im_s, dtype='float32')) # difference between shadow image and shadow_free image
-    L = threshold_otsu(diff)
-    mask = torch.tensor((np.float32(diff >= L)-0.5)/0.5).unsqueeze(0).unsqueeze(0).cuda() #-1.0:non-shadow, 1.0:shadow
-    mask.requires_grad = False
-
-    return mask
+        diff = (np.asarray(im_f, dtype='float32')- np.asarray(im_s, dtype='float32')) # difference between shadow image and shadow_free image
+        L = threshold_otsu(diff)
+        mask = torch.tensor((np.float32(diff >= L)-0.5)/0.5).unsqueeze(0).unsqueeze(0).cuda() #-1.0:non-shadow, 1.0:shadow
+        mask.requires_grad = False
+        masks.append(mask)
+    return masks
 
 def ps(t,name):
     print(f"{name}_size: {t.size()}")
