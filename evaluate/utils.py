4e6e21c26ca9c15352631bfe034e4bb83e080f0f
lekynam2000 <lekynam2000@gmail.com>
2022-12-31 11:06:50 +0800

    Succeed: evaluate

--- evaluate/utils.py
+++ evaluate/utils.py
@@ -3,29 +3,32 @@
 import argparse
 import sys
 import os
+from tqdm import tqdm
 
 from skimage.metrics import mean_squared_error as mse, peak_signal_noise_ratio as psnr, structural_similarity as ssim
 from PIL import Image
+from numpy import asarray
 import math
 from statistics import mean
 
 def compare(gt,est):
     result=dict()
-    gt_img = Image.open(gt).convert('RGB')
-    est_img = Image.open(est).convert('RGB') 
+    gt_img = asarray(Image.open(gt).convert('RGB'))
+    est_img = asarray(Image.open(est).convert('RGB'))
+    print(gt_img.shape) 
     result["rmse"] = math.sqrt(mse(gt_img,est_img))
     result["psnr"] = psnr(gt_img,est_img)
-    result["ssim"] = ssim(gt_img,est_img,channel_axis=0)
+    result["ssim"] = ssim(gt_img,est_img,channel_axis=2)
     return result
 
 def evaluate(gt_dir,est_dir,img_suf=".png"):
     gt_list = [os.path.splitext(f)[0] for f in os.listdir(gt_dir) if f.endswith(img_suf)]
     est_set = set([os.path.splitext(f)[0] for f in os.listdir(est_dir) if f.endswith(img_suf)])
     metrics_list={"rmse":[],"psnr":[],"ssim":[]}
-    for idx, img_name in enumerate(gt_list):
+    for idx, img_name in tqdm(enumerate(gt_list)):
         if img_name in est_set:
             gt = os.path.join(gt_dir,img_name+img_suf)
-            est = os.path.join(est_dir,img_name+img_sur)
+            est = os.path.join(est_dir,img_name+img_suf)
             metrics = compare(gt,est)
             for m in metrics:
                 metrics_list[m].append(metrics[m]) 
